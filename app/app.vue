caf1039c846a64ff3fe6423117605b556fb76af5
Kazuma Kusu <xkzm@camphora.tech>
2025-03-11 17:34:27 +0900

    refactor: SEO meta を変更

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 useSeoMeta({
-  title: 'NuxtHub Starter',
-  description: 'A Nuxt template to build your full-stack application on the edge.'
-})
+  title: "AI協調型療育支援システム コグロウ",
+  description:
+    "AI協調型療育支援システム「コグロウ」のホームページです。療育支援業務をコグロウ（AI）と協力することで、効率化・高品質化を支援します！",
+});
 </script>
 
 <template>
